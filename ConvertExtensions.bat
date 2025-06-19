4a28757f17e88d76630271c33606d856ac3b6d5a
Ioxcv12 <160195069+Ioxcv12@users.noreply.github.com>
2025-06-19 00:52:03 -0700

    Create ConvertExtensions.bat

---
+++ ConvertExtensions.bat
@@ -0,0 +1,10 @@
+rem This batch file renames endorphin files by giving them Learning Edition extensions.
+
+rem Rename endorphin Scene Files
+RENAME *.ens *.els
+
+rem Rename endorphin Character Files
+RENAME *.nmc *.elc
+
+rem Rename endorphin Pose Files
+RENAME *.nma *.elp
